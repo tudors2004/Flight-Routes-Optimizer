677dfff7222f71785e6ab7b8f230da01864ce566
Tudor Sârghiuță <149654961+tudors2004@users.noreply.github.com>
2025-03-16 12:25:11 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -13,22 +13,24 @@ def test_home_page(client):
     assert response.status_code == 200
     assert b"<html" in response.data
 
+@patch('app.get_db_connection')
 @patch('database.return_db_connection')
-@patch('database.get_db_connection')
-def test_airports_endpoint(mock_get_db_conn, mock_return_db_conn, client):
+def test_airports_endpoint(mock_return_db_conn, mock_get_db_conn, client):
     mock_conn = MagicMock()
     mock_cursor = MagicMock()
-    
+
     mock_cursor.fetchall.return_value = [
         ("Henri Coandă Airport", "Bucharest", "OTP", "Romania"),
         ("Heathrow Airport", "London", "LHR", "UK"),
     ]
-    
+
     mock_conn.cursor.return_value = mock_cursor
     mock_get_db_conn.return_value = mock_conn
+
     response = client.get('/airports')
     assert response.status_code == 200
     assert response.is_json
+
     data = response.get_json()
     assert isinstance(data, list)
     assert data[0]['IATA Code'] == "OTP"
