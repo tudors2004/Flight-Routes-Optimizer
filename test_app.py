880b433bdcdf9cdea21b2faa1421229330ea34d1
Tudor Sârghiuță <149654961+tudors2004@users.noreply.github.com>
2025-03-16 12:15:21 +0200

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import pytest
+from app import app 
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True 
+    with app.test_client() as client:
+        yield client
+
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"<html" in response.data 
+
+def test_airports_endpoint(client):
+    response = client.get('/airports')
+    assert response.status_code == 200
+    assert response.is_json
+    assert isinstance(response.get_json(), list)
