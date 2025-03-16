559eb8ede4c16fccec8c50cad344924c0729466f
Tudor Sârghiuță <149654961+tudors2004@users.noreply.github.com>
2025-03-16 12:20:35 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest.mock import patch, MagicMock
 from app import app 
 
 @pytest.fixture
@@ -7,14 +8,27 @@ def client():
     with app.test_client() as client:
         yield client
 
-
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b"<html" in response.data 
 
-def test_airports_endpoint(client):
+@patch("database.get_db_connection") 
+@patch("database.return_db_connection")  
+def test_airports_endpoint(mock_get_db_conn, mock_return_db_conn, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+
+    mock_cursor.fetchall.return_value = [
+        ("Henri Coandă Airport", "Bucharest", "OTP", "Romania"),
+        ("Heathrow Airport", "London", "LHR", "UK"),
+    ]
+
+    mock_conn.cursor.return_value = mock_cursor
+    mock_get_db_conn.return_value = mock_conn
     response = client.get('/airports')
     assert response.status_code == 200
     assert response.is_json
-    assert isinstance(response.get_json(), list)
+    data = response.get_json()
+    assert isinstance(data, list)
+    assert data[0]['IATA Code'] == "OTP"
