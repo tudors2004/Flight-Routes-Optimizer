4c10e66565a6ae51a5963d4bc6525bb1c1d6efde
Tudor Sârghiuță <149654961+tudors2004@users.noreply.github.com>
2025-03-16 12:22:44 +0200

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,29 +1,29 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from app import app 
+from app import app
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True 
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"<html" in response.data 
+    assert b"<html" in response.data
 
-@patch("database.get_db_connection") 
-@patch("database.return_db_connection")  
+@patch('database.return_db_connection')
+@patch('database.get_db_connection')
 def test_airports_endpoint(mock_get_db_conn, mock_return_db_conn, client):
     mock_conn = MagicMock()
     mock_cursor = MagicMock()
-
+    
     mock_cursor.fetchall.return_value = [
         ("Henri Coandă Airport", "Bucharest", "OTP", "Romania"),
         ("Heathrow Airport", "London", "LHR", "UK"),
     ]
-
+    
     mock_conn.cursor.return_value = mock_cursor
     mock_get_db_conn.return_value = mock_conn
     response = client.get('/airports')
